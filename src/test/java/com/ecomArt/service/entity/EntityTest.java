d8add19ef9cf7c738e7e5f3c47c37d0215791208
cathymanchala <149958831+cathymanchala@users.noreply.github.com>
2023-11-07 13:24:06 -0600

    Update EntityTest.java

--- src/test/java/com/ecomArt/service/entity/EntityTest.java
+++ src/test/java/com/ecomArt/service/entity/EntityTest.java
@@ -1,11 +1,6 @@
 package com.ecomArt.service.entity;
 
-import com.ecomArt.service.entity.ArtistAccount;
-import com.ecomArt.service.entity.GeneralPublicUser;
-import com.ecomArt.service.entity.Product;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class EntityTest {
@@ -13,51 +8,40 @@ public class EntityTest {
     @Test
     public void testArtistAccount() {
         ArtistAccount artistAccount = new ArtistAccount();
-        String password = "testPassword";
-        String name = "testName";
-        String email = "testEmail";
 
-        assertEquals(password, "testPassword");
-        assertEquals(name, "testName");
-        assertEquals(email, "testEmail");
+        artistAccount.setPassword("testPassword");
+        artistAccount.setName("testUser");
+        artistAccount.setEmail("testUser@test.io");
+
+        assertNotNull(artistAccount);
     }
 
     @Test
     public void testGeneralPublicUser() {
         GeneralPublicUser generalPublicUser = new GeneralPublicUser();
-        String password = "testPassword";
-        String emailId = "testEmailId";
-        String name = "testName";
-        String phoneNumber = "1";
 
-        assertEquals(password, "testPassword");
-        assertEquals(emailId, "testEmailId");
-        assertEquals(name, "testName");
-        assertEquals(phoneNumber, "1");
+        generalPublicUser.setPassword("testPassword");
+        generalPublicUser.setEmailId("testUser@test.io");
+        generalPublicUser.setName("testUser");
+        generalPublicUser.setPhoneNumber("0102030");
+
+        assertNotNull(generalPublicUser);
     }
 
     @Test
     public void testProduct() {
         Product product = new Product();
 
-        String productName = "testProductName";
-        double price = 1.01;
-        String description = "testDescription";
-        byte[] imageData = new byte[]{1};
-        String imageName = "testImageName";
-        String imageType = "testImageType";
-        double discount = 1.5;
-        int stock = 10;
-        String category = "testCategory";
+        product.setProductName("testUser");
+        product.setPrice(1.01);
+        product.setDescription("testDescription");
+        product.setImageData(new byte[]{1});
+        product.setImageName("testImageName");
+        product.setImageType("testImageType");
+        product.setDiscount(15.0);
+        product.setStock(10);
+        product.setCategory("testCategory");
 
-        assertEquals(productName, "testProductName");
-        assertEquals(price, 1.01);
-        assertEquals(description, "testDescription");
-        assertNotNull(imageData);
-        assertEquals(imageName, "testImageName");
-        assertEquals(imageType, "testImageType");
-        assertEquals(discount, 1.5);
-        assertEquals(stock, 10);
-        assertEquals(category, "testCategory");
+        assertNotNull(product);
     }
 }
